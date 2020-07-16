19491629c7635800b2ddc715bdd838ec5bf6f659
JatinderSingh1981 <59826803+JatinderSingh1981@users.noreply.github.com>
2020-07-16 17:13:52 +1000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //apiUrl: 'http://localhost:55217/api'
-  apiUrl:'https://folio1apptest20200618000446.azurewebsites.net/api'
+  apiUrl:'https://folioapi.azurewebsites.net/api'
 };
